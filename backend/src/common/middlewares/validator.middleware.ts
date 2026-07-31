7886e2498841a9139d299944d025ae1e281aa090
manishkarki02 <manishkarki.dev@gmail.com>
2026-07-31 15:35:13 +0545

    feat: enhance validation middleware with async parsing and add validated property to request

--- backend/src/common/middlewares/validator.middleware.ts
+++ backend/src/common/middlewares/validator.middleware.ts
@@ -1,47 +1,28 @@
 import type { NextFunction, Request, Response } from "express";
-import { ZodError, type ZodObject } from "zod/v4";
+import type { ZodObject } from "zod";
 import { formatError } from "../utils/error.utils";
 import { BadRequestError } from "../utils/errorClass.utils";
 
-// ---------------------------- Combined Zod Schema Type ---------------------------- //
-interface CombinedSchema extends ZodObject {
-	body?: ZodObject;
-	params?: ZodObject;
-	query?: ZodObject;
-	file?: ZodObject;
-}
-
-// ---------------------------- Validation Middleware ---------------------------- //
-export const validationMiddleware = (schema: CombinedSchema) => {
+export const validationMiddleware = <TSchema extends ZodObject>(schema: TSchema) => {
 	return async (req: Request, _res: Response, next: NextFunction) => {
 		try {
-			const result = schema.safeParse({
+			const result = await schema.safeParseAsync({
 				params: req.params,
 				query: req.query,
 				body: req.body,
 				file: req.file,
+				files: req.files,
 			});
 
 			if (!result.success) {
-				return next(new BadRequestError("Validation Error", formatError(result.error)));
-			}
-			(["body", "params", "query", "file"] as const).forEach((key) => {
-				if (req[key]) {
-					Object.defineProperty(req, key, {
-						value: result.data[key],
-						writable: true,
-						configurable: true,
-						enumerable: true,
-					});
-				}
-			});
-			next();
-		} catch (error) {
-			if (error instanceof ZodError) {
-				return next(new BadRequestError("Validation error", formatError(error)));
+				return next(new BadRequestError("Validation error", formatError(result.error)));
 			}
 
-			return next(new BadRequestError("Unexpected validation error"));
+			req.validated = result.data;
+
+			return next();
+		} catch (error) {
+			return next(error);
 		}
 	};
 };
