c6dc4495cdbfdf2a017b03c43c61f3d95fdde248
manishkarki02 <manishkarki.dev@gmail.com>
2026-08-06 08:47:27 +0545

    refactor(kids): improve code formatting and structure for readability

--- backend/src/features/kids/kids.service.ts
+++ backend/src/features/kids/kids.service.ts
@@ -1,4 +1,7 @@
-import { InternalServerError, NotFoundError } from "@/common/utils/errorClass.utils";
+import {
+  InternalServerError,
+  NotFoundError,
+} from "@/common/utils/errorClass.utils";
 import buildPrismaQuery from "@/common/utils/query.utils";
 import Environment from "@/config/env.config";
 import { kidsAllQueryConfig, kidsQueryConfig } from "@/config/query.config";
@@ -6,180 +9,196 @@ import { prisma } from "@/db";
 import { softDeleteKidWithRelations } from "@/features/kids/kids.repository";
 import { Role } from "@/generated/prisma/enums";
 import type {
-	CreateKidRequestSchema,
-	GetAllKidsRequestSchema,
-	GetKidsRequestSchema,
-	UpdateKidDetailsRequestSchema,
+  CreateKidRequestSchema,
+  GetAllKidsRequestSchema,
+  GetKidsRequestSchema,
+  UpdateKidDetailsRequestSchema,
 } from "./kids.schema";
+import { buildPaginationMetaData } from "@/common/utils/response.utils";
 
 // -- Create a new kid for adoption
 export const createKid = async (
-	body: CreateKidRequestSchema["body"],
-	userId: string,
-	file?: Express.Multer.File,
+  body: CreateKidRequestSchema["body"],
+  userId: string,
+  file?: Express.Multer.File,
 ) => {
-	let imageUrl = null;
-	if (file) {
-		imageUrl = `${Environment.get("API_URL")}/uploads/${file.filename}`;
-	}
+  let imageUrl = null;
+  if (file) {
+    imageUrl = `${Environment.get("API_URL")}/uploads/${file.filename}`;
+  }
 
-	const kid = await prisma.kidsForAdoption.create({
-		data: {
-			image: imageUrl,
-			name: body.name,
-			dob: body.dob,
-			gender: body.gender,
-			province: body.province,
-			description: body.description,
-			createdById: userId,
-		},
-	});
-	if (!kid) {
-		throw new InternalServerError("Failed to create kid for adoption.");
-	}
+  const kid = await prisma.kidsForAdoption.create({
+    data: {
+      image: imageUrl,
+      name: body.name,
+      dob: body.dob,
+      gender: body.gender,
+      province: body.province,
+      description: body.description,
+      createdById: userId,
+    },
+  });
+  if (!kid) {
+    throw new InternalServerError("Failed to create kid for adoption.");
+  }
 
-	return kid;
+  return kid;
 };
 
 // -- Get all kids for adoption
 export const getKids = async (query: GetKidsRequestSchema["query"]) => {
-	const { where, take, orderBy, skip } = buildPrismaQuery(query, kidsQueryConfig);
+  const { where, take, orderBy, skip } = buildPrismaQuery(
+    query,
+    kidsQueryConfig,
+  );
 
-	const kids = await prisma.kidsForAdoption.findMany({
-		where: {
-			...where,
-			isAdopted: false,
-			deletedAt: null,
-		},
-		take,
-		orderBy,
-		skip,
-		select: {
-			id: true,
-			image: true,
-			name: true,
-			dob: true,
-			gender: true,
-		},
-	});
+  const finalWhere = { ...where, isAdopted: false, deletedAt: null };
 
-	return kids;
+  const kids = await prisma.$transaction([
+    prisma.kidsForAdoption.findMany({
+      where: finalWhere,
+      take,
+      orderBy,
+      skip,
+      select: {
+        id: true,
+        image: true,
+        name: true,
+        dob: true,
+        gender: true,
+      },
+    }),
+    prisma.kidsForAdoption.count({ where: finalWhere }),
+  ]);
+
+  return kids;
 };
 
 // -- Get all kids for adoption (Admin)
 export const getAllKids = async (query: GetAllKidsRequestSchema["query"]) => {
-	const { where, take, orderBy, skip } = buildPrismaQuery(query, kidsAllQueryConfig);
+  const { where, take, orderBy, skip } = buildPrismaQuery(
+    query,
+    kidsAllQueryConfig,
+  );
+  const finalWhere = { ...where, deletedAt: null };
 
-	const kids = await prisma.kidsForAdoption.findMany({
-		where: {
-			...where,
-			deletedAt: null,
-		},
-		take,
-		orderBy,
-		skip,
-		select: {
-			id: true,
-			image: true,
-			name: true,
-			dob: true,
-			gender: true,
-			createdAt: true,
-			createdBy: {
-				select: {
-					id: true,
-					name: true,
-				},
-			},
-			updatedAt: true,
-			updatedBy: {
-				select: {
-					id: true,
-					name: true,
-				},
-			},
-		},
-	});
+  const [kids, total] = await prisma.$transaction([
+    prisma.kidsForAdoption.findMany({
+      where: finalWhere,
+      take,
+      orderBy,
+      skip,
+      select: {
+        id: true,
+        image: true,
+        name: true,
+        dob: true,
+        gender: true,
+        createdAt: true,
+        createdBy: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        updatedAt: true,
+        updatedBy: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    }),
+    prisma.kidsForAdoption.count({ where: finalWhere }),
+  ]);
 
-	return kids;
+  return {
+    data: kids,
+    pagination: buildPaginationMetaData({
+      page: query.page,
+      limit: query.limit,
+      total,
+    }),
+  };
 };
 
 // -- Get a single kid by ID
 export const getKidDetailById = async (id: string, role: Role) => {
-	const where: { id: string; isAdopted?: boolean } = { id };
-	if (role !== Role.Admin) {
-		where.isAdopted = false;
-	}
-	const kid = await prisma.kidsForAdoption.findUnique({
-		where: { ...where, deletedAt: null },
-		select: {
-			id: true,
-			image: true,
-			name: true,
-			dob: true,
-			gender: true,
-			province: true,
-			description: true,
-			createdAt: true,
-			updatedAt: true,
-			createdBy: {
-				select: {
-					id: true,
-					name: true,
-				},
-			},
-			updatedBy: {
-				select: {
-					id: true,
-					name: true,
-				},
-			},
-		},
-	});
+  const where: { id: string; isAdopted?: boolean } = { id };
+  if (role !== Role.Admin) {
+    where.isAdopted = false;
+  }
+  const kid = await prisma.kidsForAdoption.findUnique({
+    where: { ...where, deletedAt: null },
+    select: {
+      id: true,
+      image: true,
+      name: true,
+      dob: true,
+      gender: true,
+      province: true,
+      description: true,
+      createdAt: true,
+      updatedAt: true,
+      createdBy: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      updatedBy: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
 
-	if (!kid) {
-		throw new NotFoundError("Kid not found.");
-	}
-	return kid;
+  if (!kid) {
+    throw new NotFoundError("Kid not found.");
+  }
+  return kid;
 };
 
 // -- Update a kid's details
 export const updateKidDetails = async (
-	id: string,
-	userId: string,
-	data: UpdateKidDetailsRequestSchema["body"],
-	file?: Express.Multer.File,
+  id: string,
+  userId: string,
+  data: UpdateKidDetailsRequestSchema["body"],
+  file?: Express.Multer.File,
 ) => {
-	return await prisma.$transaction(async (tx) => {
-		const existingKid = await tx.kidsForAdoption.findUnique({
-			where: { id, isAdopted: false, deletedAt: null },
-		});
-		if (!existingKid) {
-			throw new NotFoundError("Kid not found.");
-		}
+  return await prisma.$transaction(async (tx) => {
+    const existingKid = await tx.kidsForAdoption.findUnique({
+      where: { id, isAdopted: false, deletedAt: null },
+    });
+    if (!existingKid) {
+      throw new NotFoundError("Kid not found.");
+    }
 
-		let imageUrl: string | undefined;
-		if (file) {
-			imageUrl = `${Environment.get("API_URL")}/uploads/${file.filename}`;
-		}
+    let imageUrl: string | undefined;
+    if (file) {
+      imageUrl = `${Environment.get("API_URL")}/uploads/${file.filename}`;
+    }
 
-		return await tx.kidsForAdoption.update({
-			where: { id },
-			data: {
-				image: imageUrl,
-				name: data.name,
-				dob: data.dob,
-				gender: data.gender,
-				province: data.province,
-				description: data.description,
-				updatedById: userId,
-			},
-		});
-	});
+    return await tx.kidsForAdoption.update({
+      where: { id },
+      data: {
+        image: imageUrl,
+        name: data.name,
+        dob: data.dob,
+        gender: data.gender,
+        province: data.province,
+        description: data.description,
+        updatedById: userId,
+      },
+    });
+  });
 };
 
 // -- Delete a Kid by Id, along with their adoption requests and home survey tasks
 export const deleteKidById = async (id: string, userId: string) => {
-	await softDeleteKidWithRelations(id, userId);
-	return;
+  await softDeleteKidWithRelations(id, userId);
+  return;
 };
